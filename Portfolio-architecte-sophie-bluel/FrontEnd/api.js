30bfe273031665d8923d8ae2f4b221b36aeaddd5
David <davidlebeau64@hotmail.com>
2023-05-07 17:23:50 +0200

    add filter and fetch

--- Portfolio-architecte-sophie-bluel/FrontEnd/api.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/api.js
@@ -1,6 +1,6 @@
 class Article {
   constructor(jsonArticle) {
-    jsonArticle && Object.assign(this, jsonArticle)
+    jsonArticle && Object.assign(this, jsonArticle);
   }
 }
 
@@ -10,14 +10,49 @@ const getImagesList = async () => {
 }
 
 fetch("http://localhost:5678/api/works")
-  .then(data => data.json())
-  .then(jsonListArticle => {
+  .then((data) => data.json())
+  .then((jsonListArticle) => {
     for (let jsonArticle of jsonListArticle) {
       let article = new Article(jsonArticle);
-      document.querySelector(".gallery").innerHTML +=
-        `<figure>
-      <img src="${article.imageUrl}" alt="${article.title}">
-      <figcaption>${article.title}</figcaption>
-      </figure>`
+      document.querySelector(".gallery").innerHTML += `
+        <figure>
+          <img src="${article.imageUrl}" alt="${article.title}">
+          <figcaption>${article.title}</figcaption>
+        </figure>`;
     }
   });
+
+let filters = Array.from(document.querySelectorAll(".filter"));
+let uniqueFilters = new Set(filters);
+
+uniqueFilters.forEach((filter) => {
+  filter.addEventListener("click", async function () {
+    uniqueFilters.forEach((f) => {
+      f.classList.remove("filter__selected");
+    });
+    this.classList.add("filter__selected");
+
+    let displayImages = parseInt(this.getAttribute("data-category-id"));
+
+    const allImages = await getImagesList();
+
+    let filteredImages = [];
+
+    if (displayImages === 0) {
+      filteredImages = allImages;
+    } else {
+      filteredImages = allImages.filter((image) => image.categoryId === displayImages);
+    }
+
+    document.querySelector(".gallery").innerHTML = "";
+
+    filteredImages.forEach((image) => {
+      let article = new Article(image);
+      document.querySelector(".gallery").innerHTML += `
+        <figure>
+          <img src="${article.imageUrl}" alt="${article.title}">
+          <figcaption>${article.title}</figcaption>
+        </figure>`;
+    });
+  });
+});
--- Portfolio-architecte-sophie-bluel/FrontEnd/filter.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/filter.js
@@ -1,47 +1,48 @@
 
 
-let filterSelected = document.querySelector(".filter__selected");
-let filters = Array.from(document.querySelectorAll(".filter"));
-let uniqueFilters = new Set(filters);
-let index = 0;
-
-uniqueFilters.forEach((filter) => {
-  filter.addEventListener("click", function () {
-    uniqueFilters.forEach((filter) => {
-      filter.classList.remove("filter__selected");
-    });
-    this.classList.add("filter__selected");
-  });
-});
-
-// function: displayImages
-const displayImages = async (categoryId) => {
-  // get images
-  const allImages = await getImagesList()
-
-  let filteredImages = [];
-
-  // if all images, set filteredImages to allImages
-  if (!categoryId) {
-    filteredImages = allImages
-  } else {  // else filter it
-
-    allImages.forEach((image) => {
-      if (image.categoryId === categoryId) {
-        filteredImages.push(image)
-      }
-    })
-  }
-  console.log(filteredImages);
-
-  // display images
-  
-  
-}
-
-displayImages()
+// let filterSelected = document.querySelector(".filter__selected");
+// let filters = Array.from(document.querySelectorAll(".filter"));
+// let uniqueFilters = new Set(filters);
 
 
-// uniqueFilters[1].addEventListener('click', () => {
-   
+
+// uniqueFilters.forEach((filter) => {
+//   filter.addEventListener("click", function () {
+//     uniqueFilters.forEach((filter) => {
+//       filter.classList.remove("filter__selected");
+//     });
+//     this.classList.add("filter__selected");
+//     // function: displayImages
+//     const displayImages = async (categoryId) => {
+//       // console.log(displayImages)
+//       // get images
+//       const allImages = await getImagesList()
+//       let filteredImages = [];
+//       console.log(filteredImages)
+
+//       // if all images, set filteredImages to allImages
+//       if (displayImages === 0) {
+//         filteredImages = allImages;
+//       } else {  // else filter it
+//         allImages.forEach((image) => {
+//           if (image.displayImages === displayImages) {
+//             filteredImages.push(image);
+            
+//           } else {
+//             delete image.displayImages;
+//           }
+//         });
+//       }
+//       // console.log(filteredImages);
+
+//       // display images
+//     }
+
+//     displayImages()
+
 //   });
+// });
+
+
+
+
