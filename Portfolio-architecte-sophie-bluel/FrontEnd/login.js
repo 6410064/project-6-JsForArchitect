0e18751da8f2a1f9424f9ed15b6e553cfcd67ebf
David <davidlebeau64@hotmail.com>
2023-05-08 23:23:52 +0200

    add-log-in 3.0

--- Portfolio-architecte-sophie-bluel/FrontEnd/login.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/login.js
@@ -1,11 +1,43 @@
 document.querySelector('#log__form input[type="submit"]').addEventListener("click", function () {
-    for (let input of document.querySelectorAll(".form input,.form label")) {
+    let valid = true;
+
+    for (let input of document.querySelectorAll(".form input, .form label")) {
         valid = valid && input.reportValidity();
-        if(!valid){
+
+        if (!valid) {
             break;
         }
-        else (valid) { 
-            alert("Message envoyé")
-        }
+    }
+
+    if (valid) {
+        let email = document.querySelector('#email').value;
+        let password = document.querySelector('#password').value;
+
+        // Préparation des données à envoyer dans la requête
+        let data = {
+            email: email,
+            password: password
+        };
+
+        // Envoi de la requête POST à l'API
+        fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(result => {
+                if (result.success) {
+                    // Redirection vers le site après la validation des identifiants
+                    window.location.href = "index.html";
+                } else {
+                    alert("Identifiants incorrects");
+                }
+            })
+            .catch(error => {
+                console.error('Erreur lors de la requête:', error);
+            });
     }
 });
